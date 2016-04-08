786e8808429a1464d17832ed4152e7c4da136bab
ahadik <ahadik@us.ibm.com>
2016-04-08 12:45:40 -0500

    a few changes

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ var express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
     hello_world = require('./modules/final/hello-world'), //for demonstration purposes only
-    fs;
+    fs,
+    multer = require('multer');
 
 var app = express(),
-    appEnv = cfenv.getAppEnv();
+    appEnv = cfenv.getAppEnv(),
+    storage = multer.diskStorage({
+    	destination: function(req, file, callback){
+    		callback(null, './uploads');
+    	}
+    });
+var upload = multer({storage : storage}).single('wireframe');
+
+app.post('/upload', function(req, res){
+	upload(req, res, function(err){
+		if(err){
+			return res.end('Error uploading file.');
+		}
+		res.end('File is uploaded');
+	});
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 //app.set(path.join('views', __dirname, '/apps'));
