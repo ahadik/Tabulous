9034527098b79ab771e42a2442a36aba7479bc19
Alexander Hadik <alex@alexhadik.com>
2016-05-24 20:57:48 -0500

    Loading indicator (#48)
    * Basic loading module complete
    
    * Loading indicator for download

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,31 +1,24 @@
-class Loader{
-	constructor(width, color, parent){
+export default class Loader{
+	constructor(width, color, parent, callback){
 		this.width = width;
 		this.color = color;
+		this.parent = parent;
+		this.generate(callback);
 	}
-	generate(){
-
+	generate(callback){
+		this.parent.append('path')
+			.attr('d', 'M6.084 27.974c.408.453.841.881 1.3 1.283a15.44 15.44 0 0 0 10.222 3.85c8.56 0 15.5-6.94 15.5-15.5 0-8.561-6.94-15.5-15.5-15.5-8.56 0-15.5 6.939-15.5 15.5')
+			.attr('class', 'icon-loading')
+			.attr('stroke-width', '3')
+			.attr('stroke', this.color)
+			.attr('fill', 'none')
+			.attr('fill-rule', 'evenodd')
+		callback(this);
+	}
+	show(){
+		this.parent[0][0].classList.add('is-loading');
+	}
+	hide(){
+		this.parent[0][0].classList.remove('is-loading');
 	}
 }
-
-/*
-<svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-	<input class="box__file" type="file" name="wireframe" id="file"/>
-	<button class="box__button" type="submit">Upload</button>
-	<g id="Afternoon-9.30.15" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-		<g id="Desktop-Copy-16" transform="translate(-318.000000, -318.000000)" fill="#FFFFFF">
-			<g id="Polygon-26-+-Start-a’fresh-Copy" transform="translate(319.000000, 319.000000)">
-				<polygon id="Polygon-26" stroke="#FFFFFF" fill-opacity="0.2" points="67 0 133.683956 38.5 133.683956 115.5 67 154 0.316043909 115.5 0.316043909 38.5 "></polygon>
-				<text id="Start-a’fresh-Copy" class="icon-add" font-family=".AppleSystemUIFont" font-size="56" font-weight="normal">
-					<tspan x="49.9375" y="91">+</tspan>
-				</text>
-				<text class="icon-error" font-family=".AppleSystemUIFont" font-size="56" font-weight="normal">
-					<tspan x="59" y="100">!</tspan>
-				</text>
-				<path class="icon-loading" d="M6.084 27.974c.408.453.841.881 1.3 1.283a15.44 15.44 0 0 0 10.222 3.85c8.56 0 15.5-6.94 15.5-15.5 0-8.561-6.94-15.5-15.5-15.5-8.56 0-15.5 6.939-15.5 15.5" stroke="#FFF" stroke-width="3" fill="none" fill-rule="evenodd"/>
-			</g>
-		</g>
-	</g>
-</svg>
-
-*/
